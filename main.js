8c7793610bea4f91c1db099c8a6b6e0956a9efee
Manuchong <you@example.com>
2022-03-11 05:33:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+/* 1- Deberán mostrar por consola los siguientes datos:
+
+a) Mostrar el día de tu cumpleaños (en numero) Ej: 23
+
+b) Mostrar el mes de tu cumpleaños (en numero) Ej: 07
+
+c) Mostrar el año de tu nacimiento.
+
+d) Mostrar tu número favorito.
+
+La entrega de la actividad puede ser en las siguientes formas:
+
+- Captura de pantalla
+
+- Archivo de html*/
+var a=("cumplo años el " + 10);
+
+console.log(a);
+
+var b=("cumplo el mes " + 9);
+
+console.log(b);
+
+var c=("Naci en el Año " + 1981);
+
+console.log(c);
+
+var d=("Mi número favorito es el " + 10);
+
+console.log(d);
+
