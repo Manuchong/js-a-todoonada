548ac69c05f5c820f7409314e127c09c61a3930b
Manuchong <85263292+Manuchong@users.noreply.github.com>
2022-03-17 17:42:26 -0300

    agregué tp2

---
+++ main2.js
@@ -0,0 +1,22 @@
+/*
+1 Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas que hay en tu casa. Mostrar el valor de la variable por la consola.*/
+var cantidadDeVentanasEnMiCasa = 5;
+console.log(cantidadDeVentanasEnMiCasa);
+
+/*
+2 Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero). Mostrar el valor de la variable por la consola.*/
+
+var soyHumano= true;
+console.log(soyHumano);
+/*
+3 Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad de pizza que más te guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.*/
+
+var miGustoDePizzaPreferido= "calabreza";
+console.log("Mi gusto de pizza preferido es "+ miGustoDePizzaPreferido);
+
+/*
+4 Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.*/
+
+var valores=["Hola","Minions",0,false];
+console.log(valores)
+
